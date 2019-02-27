1cabc5bd53ebd246fd422af8e1b7c649e1b52f61
mkolosek <marko.milosevic@kolosek.com>
2019-02-27 12:17:12 +0100

    .at parser update

--- lib/whois/parsers/whois.nic.at.rb
+++ lib/whois/parsers/whois.nic.at.rb
@@ -8,7 +8,7 @@
 
 
 require_relative 'base'
-
+require 'whois/scanners/whois.nic.at.rb'
 
 module Whois
   class Parsers
@@ -23,38 +23,107 @@ module Whois
     #   The Example parser for the list of all available methods.
     #
     class WhoisNicAt < Base
+      include Scanners::Scannable
+
+      self.scanner = Scanners::WhoisNicAt
+
+      property_supported :domain do
+        node("domain")
+      end
+
+      property_not_supported :domain_id
+
 
       property_supported :status do
-        if available?
-          :available
-        else
-          :registered
-        end
+        node("% nothing found")
       end
 
       property_supported :available? do
-        !!(content_for_scanner =~ /% nothing found/)
+        !invalid? && node("Status") == "free"
       end
 
       property_supported :registered? do
-        !available?
+        !invalid? && !available?
       end
-
 
       property_not_supported :created_on
 
       property_supported :updated_on do
-        if content_for_scanner =~ /changed:\s+(.*)\n/
-          parse_time($1)
-        end
+        node("Changed") { |value| parse_time(value) }
       end
 
       property_not_supported :expires_on
 
+      property_supported :registrar do
+        node("registrar")
+      end
 
+      property_supported :registrant_contacts do
+        build_contact("Holder", Parser::Contact::TYPE_REGISTRANT)
+      end
+
+      property_supported :admin_contacts do
+        build_contact("Admin-C", Parser::Contact::TYPE_ADMINISTRATIVE)
+      end
+
+      property_supported :technical_contacts do
+        build_contact("Tech-C", Parser::Contact::TYPE_TECHNICAL)
+      end
+
+
+      # Nameservers are listed in the following formats:
+      #
+      #   Nserver:     ns1.prodns.de. 213.160.64.75
+      #   Nserver:     ns1.prodns.de.
+      #
       property_supported :nameservers do
-        content_for_scanner.scan(/nserver:\s+(.+)\n/).flatten.map do |name|
-          Parser::Nameserver.new(:name => name)
+        node("nserver") do |values|
+          values.map do |line|
+            name, ipv4 = line.split(/\s+/)
+            Parser::Nameserver.new(name: name, ipv4: ipv4)
+          end
+        end
+      end
+
+
+      # Checks whether the response has been throttled.
+      #
+      # @return [Boolean]
+      #
+      # @example
+      #   % Error: 55000000002 Connection refused; access control limit reached.
+      #
+      def response_throttled?
+        !!node("response:throttled")
+      end
+
+      def response_error?
+        !!node("response:error")
+      end
+
+      def version
+        cached_properties_fetch :version do
+          if content_for_scanner =~ /^% Version: (.+)$/
+            $1
+          end
+        end
+      end
+
+      # NEWPROPERTY invalid?
+      def invalid?
+        cached_properties_fetch :invalid? do
+          node("Status") == "invalid" ||
+          response_error?
+        end
+      end
+
+      private
+
+      def build_contact(element, type)
+        node(element) do |raw|
+          Parser::Contact.new(raw) do |c|
+            c.type = type
+          end
         end
       end
 
